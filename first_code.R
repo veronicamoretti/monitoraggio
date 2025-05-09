3477efd84274e25e220160b9875f40d39dbf3c12
veronicamoretti <veronica83moretti@gmail.com>
2025-05-09 17:59:46 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -4,3 +4,6 @@ library(terra) # package used for remote sensing data
 setwd("C://utente/Desktop/") # caution!!! Usa slash, non backslash
 mato = rast("Matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+plotRGB(mato, r=2, g=3, b=1)
+plotRGB(mato, r=2, g=1, b=3)
+plotRGB(mato, r=1, g=2, b=3)
